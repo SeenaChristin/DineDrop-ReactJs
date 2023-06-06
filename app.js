a1dcadfb6d6548fd22c0feb561c6d0feee5fe0cc
Seena Christin <seenachristin@Seenas-MacBook-Air.local>
2023-06-06 19:21:15 +0530

    Episode-02

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
     "div",
     {id: "parent"},[
     React.createElement(
         "div",
         {id:"child"},[
-        React.createElement("h1",{},"I am an h1 tag"),
-        React.createElement("h2",{},"I am an h2 tag"),
+        React.createElement("h1",{},"This is Namaste React"),
+        React.createElement("h2",{},"by Akshay"),
       ]),
       React.createElement(
         "div",
         {id:"child"},[
-        React.createElement("h1",{},"I am an h1 tag"),
-        React.createElement("h2",{},"I am an h2 tag"),
+        React.createElement("h1",{},"This is Namaste React"),
+        React.createElement("h2",{},"by Seena"),
       ]),
     ]
     );
